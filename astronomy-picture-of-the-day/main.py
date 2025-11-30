068fd4f73c80cc194500e8b4af428e7d0ebd7942
benharmonics <benjamin.jiron+git@gmail.com>
2025-11-30 16:27:30 -0700

    refactor: simplify APOD code a lot and remove BeautifulSoup4 dependency

--- astronomy-picture-of-the-day/main.py
+++ astronomy-picture-of-the-day/main.py
@@ -1,98 +1,61 @@
 """Downloads Astronomy Picture of the Day into a directory .../apod/[yyyy-mm] using one of NASA's public APIs"""
 
 import datetime
+import json
 import os
-import re
-from dataclasses import dataclass
 import sys
 
 import requests
 import wget
-from bs4 import BeautifulSoup
 
 
-def _page_text() -> str:
-    """Gets raw API text and removes extraneous characters"""
-    page = requests.get("https://api.nasa.gov/planetary/apod", {"api_key": "DEMO_KEY"})
-    soup = BeautifulSoup(page.content, "html.parser")
-    # We're using an API, so we're just using everything on the page.
-    return re.sub('[{}\n"]', "", soup.text)
+def _page_json() -> dict:
+    """Get Astronomy Picture Of the Day (APOD) JSON response from API"""
+    # No API key is actually required for APOD unless you send a lot of requests
+    headers = {"api_key": "DEMO_KEY"}
+    res = requests.get("https://api.nasa.gov/planetary/apod", headers)
+    res.raise_for_status()
+    return res.json()
 
 
-def _parse_page_text(page_text: str) -> list[str]:
-    """Parses page text into a vector where each element is `[category]:[content]`"""
-    # `split(',')` creates a problem where the `explanation` category is divided into multiple sections
-    info = page_text.split(",")
-
-    # beginning of `explanation` section starts with 'explanation:'
-    explanation_i = [i for i, line in enumerate(info) if "explanation:" in line][0]
-    # end of `explanation` section ends at the `hdurl` section
-    explanation_f = [i for i, line in enumerate(info) if "hdurl:" in line][0] - 1
-    # collecting the `explanation` section into a single string
-    explanation = "".join([line for line in info[explanation_i:explanation_f]])
-
-    # correcting the split(',') problem by subbing our `explanation` string and getting rid of extra lines
-    result = [line for line in info if ":" in line.split()[0]]
-    result[explanation_i] = explanation
-
-    return result
-
-
-@dataclass
-class APODInfo:
-    url: str
-    title: str
-    extension: str
-    info: list[str]
-
-
-def _apod_info(page_text: str) -> APODInfo:
-    # Using regex to find image URL from full text
-    url = re.search("https://apod\.nasa\.gov.*?\.[a-z]{3}", page_text)
-    if not url:
-        raise ValueError("Image URL not found")
-    url = url[0]
-
-    # Getting filename from url, e.g. `example.jpg`
-    filename = re.search("[A-Za-z0-9_-]*\.[a-z]{3}$", url)
-    if not filename:
-        raise ValueError("Filename not parsed from URL")
-    filename = filename[0]
-
-    # i.e. `example.jpg` => `example`, `jpg`
-    title, extension = filename.split(".")
-
-    # We have to clean the info a bit
-    info = _parse_page_text(page_text)
-
-    return APODInfo(url=url, title=title, extension=extension, info=info)
-
-
-def download(apod_info: APODInfo, destination_dir: str):
-    def current_month() -> str:
-        date = str(datetime.date.today())
-        y, m, _ = date.split("-")
-        return f"{y}-{m}"
-
-    download_path = os.path.join(destination_dir, "apod")
-    subdir = os.path.join(download_path, current_month())
+def _download_dir(basedir: str) -> str:
+    """
+    Ensure download path exists, making one or two directories as needed.
+    Files will be downloaded e.g. <basedir>/apod/<month>/<image>.jpg
+    """
+    download_path = os.path.join(basedir, "apod")
+    subdir = os.path.join(download_path, datetime.datetime.now().strftime("%Y-%m"))
     os.makedirs(subdir, exist_ok=True)
+    return subdir
 
-    image_path = os.path.join(subdir, f"{apod_info.title}.{apod_info.extension}")
-    info_path = os.path.join(subdir, f"{apod_info.title}")
 
-    wget.download(apod_info.url, image_path)
+def _download(page_json: dict, download_dir: str) -> None:
+    """
+    Given an API response and a download destination directory, downloads
+    the Astronomy Picture Of the Day and its corresponding metadata.
+    """
+    download_url = page_json["hdurl"]
+    basename = os.path.basename(download_url)
+    file_name, _ = os.path.splitext(basename)
+    image_path = os.path.join(download_dir, basename)
+    info_path = os.path.join(download_dir, f"{file_name}.json")
+
+    wget.download(download_url, image_path)
     with open(info_path, "w+") as f:
-        f.writelines("%s\n" % line for line in apod_info.info)
+        json.dump(page_json, f)
 
 
 def main():
-    destination_dir = os.path.dirname(__file__)
+    download_dir = _download_dir(os.path.dirname(__file__))
     try:
-        apod_info = _apod_info(_page_text())
-        download(apod_info, destination_dir)
+        page_json = _page_json()
+    except requests.HTTPError as e:
+        print(f"Failed to query API: {e}", file=sys.stderr)
+        sys.exit(1)
+    try:
+        _download(page_json, download_dir)
     except ValueError as e:
-        print(f"Failure: {e}")
+        print(f"Failed to download image: {e}", file=sys.stderr)
         sys.exit(1)
 
 
